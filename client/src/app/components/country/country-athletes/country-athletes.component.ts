7669aa4b0c48ef94c545f803f5dda366f9f26eed
Diedinium <jakethomashall@gmail.com>
2022-04-12 00:20:39 +0100

    Continued work on implementing country athlete summary table

--- client/src/app/components/country/country-athletes/country-athletes.component.ts
+++ client/src/app/components/country/country-athletes/country-athletes.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { CountryAthletesResponse } from 'src/app/models/responses/country-athletes.response.model';
 import { StyleService } from 'src/app/services/style.service';
 import am5themes_Dark from '@amcharts/amcharts5/themes/Dark';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
-import { Legend, p50, Root, Tooltip } from '@amcharts/amcharts5';
+import { array, Bullet, Color, color, Label, Legend, p100, p50, percent, Root, Tooltip } from '@amcharts/amcharts5';
 import { XYChart } from '@amcharts/amcharts5/.internal/charts/xy/XYChart';
 import { CategoryAxis } from '@amcharts/amcharts5/.internal/charts/xy/axes/CategoryAxis';
 import { AxisRendererY } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRendererY';
 import { ValueAxis } from '@amcharts/amcharts5/.internal/charts/xy/axes/ValueAxis';
 import { AxisRendererX } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRendererX';
+import { ColumnSeries } from '@amcharts/amcharts5/.internal/charts/xy/series/ColumnSeries';
+import { AxisRenderer } from '@amcharts/amcharts5/.internal/charts/xy/axes/AxisRenderer';
+import { CountryService } from 'src/app/services/country.service';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-country-athletes',
@@ -21,17 +25,22 @@ export class CountryAthletesComponent implements OnInit {
 
   countryAthletesResponse: CountryAthletesResponse | null = null;
   countryAthletesOptionsForm: FormGroup;
-  error?: string; 
+  error?: string;
   hasNoAthletes = false;
 
   root!: Root;
   chart!: XYChart;
+  data: any[] = [];
+  xAxis!: CategoryAxis<AxisRenderer>;
+  yAxis!: ValueAxis<AxisRenderer>;
+  columnSeries: ColumnSeries[] = [];
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private styleService: StyleService) 
-  {
+    private countryService: CountryService,
+    private route: ActivatedRoute,
+    private styleService: StyleService) {
     this.countryAthletesOptionsForm = this.fb.group({
       weighting: [null],
       gender: [null]
@@ -44,29 +53,45 @@ export class CountryAthletesComponent implements OnInit {
     this.chart = this.root.container.children.push(XYChart.new(this.root, {
       panX: false,
       panY: false,
-      wheelX: 'panY',
-      wheelY: 'zoomY',
       layout: this.root.verticalLayout
     }));
 
-    let yAxis = this.chart.yAxes.push(CategoryAxis.new(this.root, {
+    let xRenderer = AxisRendererX.new(this.root, { minGridDistance: 0});
+    xRenderer.labels.template.setAll({
+      rotation: -40,
+      centerY: p50,
+      centerX: p100
+    });
+
+    this.xAxis = this.chart.xAxes.push(CategoryAxis.new(this.root, {
       categoryField: 'AthleteFullName',
-      renderer: AxisRendererY.new(this.root, {}),
+      renderer: xRenderer,
       tooltip: Tooltip.new(this.root, {})
     }));
 
-    let xAxis = this.chart.xAxes.push(ValueAxis.new(this.root, {
+    this.yAxis = this.chart.yAxes.push(ValueAxis.new(this.root, {
       min: 0,
-      renderer: AxisRendererX.new(this.root, {})
+      renderer: AxisRendererY.new(this.root, {})
     }));
 
-    let legend = this.chart.children.push(Legend.new(this.root, {
-      centerX: p50,
-      x: p50
-    }));
+    this.columnSeries.push(this.createSeries('Bronze', 'Bronze', color(0xE67E22)));
+    this.columnSeries.push(this.createSeries('Silver', 'Silvers', color(0xC0C0C0)));
+    this.columnSeries.push(this.createSeries('Gold', 'Golds', color(0xFFD700)));
+
+    this.chart.appear(1000, 100);
 
     this.countryAthletesOptionsForm.valueChanges.subscribe(value => {
-      this.router.navigate([], {queryParams: value});
+      this.router.navigate([], { queryParams: value });
+    });
+
+    combineLatest({params: this.route.params, queryParams: this.route.queryParams}).subscribe(value => {
+      this.countryService.getTopAthletes(value.params['id']).subscribe(result => {
+        this.xAxis.data.setAll(result.data.athletes);
+        this.columnSeries.forEach(column => {
+          column.data.setAll(result.data.athletes);
+        });
+        this.countryAthletesResponse = result;
+      });
     });
 
     this.styleService.isDarkTheme.subscribe(value => {
@@ -84,4 +109,46 @@ export class CountryAthletesComponent implements OnInit {
     });
   }
 
+  private createSeries(name: string, valueField: string, colour: Color): ColumnSeries {
+    let series = this.chart.series.push(ColumnSeries.new(this.root, {
+      name: name,
+      stacked: true,
+      xAxis: this.xAxis,
+      yAxis: this.yAxis,
+      valueYField: valueField,
+      categoryXField: 'AthleteFullName',
+      fill: colour
+    }));
+
+    series.columns.template.setAll({
+      tooltipText: '{name}, {categoryX}: {valueY}',
+      tooltipY: percent(10)
+    });
+    series.appear();
+
+    series.bullets.push(() => {
+      return Bullet.new(this.root, {
+        sprite: Label.new(this.root, {
+          text: '{valueY}',
+          fill: this.root.interfaceColors.get('alternativeText'),
+          centerY: p50,
+          centerX: p50,
+          populateText: true
+        })
+      });
+    });
+
+    series.columns.template.onPrivate('height', (height, target) => {
+      array.each(target!.dataItem!.bullets!, (bullet) => {
+        if (height! > 0) {
+          bullet.get('sprite').show();
+        }
+        else {
+          bullet.get('sprite').hide();
+        }
+      });
+    });
+
+    return series;
+  }
 }
